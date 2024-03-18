ff13c1214b8da0c6cc597d57e2562f93f4b97950
rleitenberger <rleitenberger@directecllc.com>
2024-03-18 15:38:52 -0400

    testing

--- app/api/auth/signup/route.ts
+++ app/api/auth/signup/route.ts
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = "force-no-store";
+
 import { hashBcrypt } from "@/lib/bcrypt";
 import { randomUUID } from "crypto";
 import { NextRequest } from "next/server";
