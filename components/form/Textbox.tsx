4ab19e79f86d520d7e747e564e15d1af2143a580
rleitenberger <rleitenberger@directecllc.com>
2024-03-22 08:56:29 -0400

    fix

--- components/form/Textbox.tsx
+++ components/form/Textbox.tsx
@@ -1,6 +1,8 @@
 import { DropDownSearchOption } from "@/types/DropDownSearchOption";
 import { DropDownDisplayGroup, DropDownValueFunctionGroup } from "@/types/dropDown";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+
+const MAX_FIELD_LENGTH = 200;
 
 export default function Textbox ({ fn, displayOptions }: {
     fn: DropDownValueFunctionGroup
@@ -9,12 +11,17 @@ export default function Textbox ({ fn, displayOptions }: {
 
     const [val, setVal] = useState<string>('');
 
-    const updateValue = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    const updateValue = useCallback((e: React.ChangeEvent<HTMLInputElement>): void => {
+        if (e.target.value.length >= MAX_FIELD_LENGTH){
+            return;
+        }
+
+        setVal(e.target.value);
         fn.onChange({
             name: displayOptions.name,
             value: e.target.value
         }, displayOptions.name);
-    }
+    }, [displayOptions.name, fn]);
     
     return (
         <div className="text-sm">
@@ -24,6 +31,11 @@ export default function Textbox ({ fn, displayOptions }: {
             <div>
                 <input className="px-2 py-1 border border-slate-300 rounded-lg"
                     value={val} onChange={updateValue} />
+                <div className="flex items-center gap-2 ml-auto text-slate-600 font-medium text-xsf">
+                    {val.length}
+                    /
+                    {MAX_FIELD_LENGTH}
+                </div>
             </div>
         </div>
     )
